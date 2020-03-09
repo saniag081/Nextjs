2f7681ecdf63f455439b511f3ad6972a67b06074
saniag081 <sgonzalezhernandez081@gmail.com>
2020-03-08 21:50:53 -0500

    getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,66 @@
-const holaMundo = () => {
+import 'isomorphic-fetch';
+
+const Page = ({ channels }) => {
     return(
         <div>
-            <h1>Hola mundo</h1>
-            <p>Bienvenido al curso de nextJS</p>
+            <header className="header">Podcast</header>
+
+            <div className="channels">
+                {channels.map( (channel) => (
+                    <div className="channel">
+                        <img src={channel.urls.logo_image.original} alt=""/>
+                        <h2 className="channel-title">{channel.title}</h2>
+                    </div>
+                )) }
+            </div>
 
 
             <style jsx>{`
-                h1{
-                    color: red;
+                .header{
+                    color: #fff;
+                    background: #8756ca;
+                    padding: 15px;
+                }
+                .channel{
+                    display: block;
+                    border-radius: 3px;
+                    box-shadow: 0 2px 6px rgba(0,0.0.0.15);
+                    margin-bottom: 0.5em;
+                }
+                .channels{
+                    display: grid;
+                    grid-gap: 15px;
+                    padding: 15px;
+                    grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+                }
+                .channel img{
+                    width: 100%;
+                }
+                .channel-title{
+                    padding: 5px;
+                    font-size: 0.9em;
+                    font-weight: 600;
+                    margin: 0;
+                    text-align: center;
+                }
+            `}</style>
+            <style jsx global>{`
+                body{
+                    text-align: center;                    
+                    margin: 0;
+                    font-family: system-ui;
+                    background: white;
                 }
             `}</style>
         </div>
     )
 }
 
-export default holaMundo;
+Page.getInitialProps = async (ctx) => {
+    let request = await fetch('https://api.audioboom.com/channels/recommended');
+    let { body: channels } = await request.json();
+
+    return { channels }
+}
+
+export default Page;
